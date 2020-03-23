914c61c0c9868f5243bb59062d322efa65846739
Payvand Moaddel <payvand.moaddel@genesys.com>
2020-03-22 22:44:51 -0500

    remove unnecessary code from the item reducer

--- src/app/item.reducer.ts
+++ src/app/item.reducer.ts
@@ -5,17 +5,7 @@ import Item from './item';
 
 export const initialState: Item[] = [];
 
-const TEST_ITEM = new Item({
-  "id": "001",
-  "name": "16lb bag of Skittles",
-  "price": 16.00,
-  "isImported": false,
-  "types": [ "candy" ],
-  "image": "/assets/skittles.jpg"
-});
-
 const _itemReducer = createReducer(initialState,
-  // on(getAll, state => [TEST_ITEM]), // TODO
   on(loaded, (state, { items }) => items )
 );
 
